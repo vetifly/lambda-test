2548a778131330537eea6bea3c994fa27cd9303b
sandeshgi <98804213+sandeshgi@users.noreply.github.com>
2022-03-03 15:43:41 +0530

    Update index.js
    updated

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-var AWS = require('aws-sdk');
-var lambda = new AWS.Lambda();
+const aws = require("aws-sdk");
+const ses = new aws.SES({ region: "us-east-1" });
+exports.handler = async function (event) {
+  try {
+    let eventbody=JSON.parse(event.body)
+  console.log('EVENT: ', eventbody)
+  const params = {
+    Destination: {
+      ToAddresses: [eventbody.senderEmail],
+    },
+    Message: {
+      Body: {
+        Text: { 
+            Data: eventbody.message 
+        },
+      },
+      Subject: { Data: `Message from AWS Lambda` },
+    },
+    Source: "sandeshverma05@gmail.com",
+  };
 
-var params = {}; // define your parameters
-
-lambda.createFunction(params, function(err, data) {
-    if (err && err.code === 'InvalidParameterValueException') {
-
-        // try again after a few seconds
-        setTimeout(function(){
-            lambda.createFunction(params, callback);
-        }, 10000);
-    } else {
-        callback(err, data);
-    }
-});
+  let rs=await ses.sendEmail(params).promise()
+  let CORS_HEADERS= {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-': '',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Request-Method': '*',
+        'Access-Control-Allow-Methods': '*',
+        'node-cache': 'Missed node-cache',
+        'crossDomain': true
+  }
+  
+  return {
+        statusCode: 200,
+        headers: CORS_HEADERS,
+        body: JSON.stringify(rs,
+            null,
+            2
+        )
+    };
+    
+  }
+  catch (err){
+    console.log("here is error",err)
+  }
+};
